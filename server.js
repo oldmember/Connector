f72b91cdf57acf45ef7eee0d8c448392d16d4976
truongtamgio <truongtamgio@gmail.com>
2019-08-12 18:03:32 +0700

    Minor Changes

--- server.js
+++ server.js
@@ -11,5 +11,7 @@ app.use(express.json({extended: false }))
 app.get('/', (req, res) => res.send("api drunning"))
 
 app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log('server start on '+ PORT))
