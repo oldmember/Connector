bf31087c9e6293fa8fb3dbe490cdb8f0c18bcc46
truongtamgio <truongtamgio@gmail.com>
2019-08-12 17:53:29 +0700

    [Users.js] Add jsonwebtoken feature

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,8 @@ const {check, validationResult} = require('express-validator')
 const gravatar = require('gravatar')
 const User  = require('../../models/User')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
+const config = require('config')
 //@route post api/users
 //@desc Register User
 //@access public
@@ -53,9 +54,23 @@ router.post(
             user.password = await bcrypt.hash(password, salt)
             await user.save()
 
-            res.send('user registered')
             //return jsonwebtoken
+            const payload = {
+                user: {
+                    id:user.id
+                }
+            }
 
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token) => {
+                    if (err) throw err
+                    res.json({token})
+                }
+            )
+            //res.send('user registered') // why server doesn't send token obj back since has this line
         } catch(err) {
             console.error(err.message)
             res.status(500).send('Server error')
